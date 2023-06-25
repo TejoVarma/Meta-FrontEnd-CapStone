da73b0c8c0929529877ddce2cc7f8a2c323ac92c
TejoVarma <tejovarma.alluri9@gmail.com>
2023-06-25 18:11:19 +0530

    third commit

---
+++ src/components/Footer.js
@@ -0,0 +1,44 @@
+function Footer() {
+    return (
+        <footer>
+            <div>
+                <img id="footer-logo" src="/src/components/assets/Logo Vertical.jpg" alt="" />
+            </div>
+            <div className="footer-list">
+                <h3>Doormat Navigation</h3>
+                <ul>
+                    <li><a href="/" target="_blank">Home</a></li>
+                    <li><a href="/" target="_blank">About</a></li>
+                    <li><a href="/" target="_blank">Menu</a></li>
+                    <li><a href="/" target="_blank">Reservations</a></li>
+                    <li><a href="/" target="_blank">Order Online</a></li>
+                    <li><a href="/" target="_blank">Login</a></li>
+                </ul>
+            </div>
+            <div className="footer-list">
+                <h3>Doormat Navigation</h3>
+                <ul>
+                    <li><a href="/" target="_blank">Home</a></li>
+                    <li><a href="/" target="_blank">About</a></li>
+                    <li><a href="/" target="_blank">Menu</a></li>
+                    <li><a href="/" target="_blank">Reservations</a></li>
+                    <li><a href="/" target="_blank">Order Online</a></li>
+                    <li><a href="/" target="_blank">Login</a></li>
+                </ul>
+            </div>
+            <div className="footer-list">
+                <h3>Doormat Navigation</h3>
+                <ul>
+                    <li><a href="/" target="_blank">Home</a></li>
+                    <li><a href="/" target="_blank">About</a></li>
+                    <li><a href="/" target="_blank">Menu</a></li>
+                    <li><a href="/" target="_blank">Reservations</a></li>
+                    <li><a href="/" target="_blank">Order Online</a></li>
+                    <li><a href="/" target="_blank">Login</a></li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom'
+
+function Header() {
+    return (
+        <header>
+            <img id="logo" src="assets/Logo.svg" alt="" />
+            <ul id="navbar">
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <a href="/" target="_blank">About</a>
+                </li>
+                <li>
+                    <a href="/" target="_blank">Menu</a>
+                </li>
+                <li>
+                    <Link to='/booking'>Reservations</Link>
+                </li>
+                <li>
+                    <a href="/" target="_blank">Order Online</a>
+                </li>
+                <li>
+                    <a href="/" target="_blank">Login</a>
+                </li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
